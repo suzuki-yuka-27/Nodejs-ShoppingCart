0bf06214748a01a9324d26467317dbc548bd9518
suzuki-yuka-27 <yukasuzuki@777noMacBook-Pro.local>
2026-06-30 10:05:28 +0900

    jQueryで数量変更機能を実装

--- public/js/app.js
+++ public/js/app.js
@@ -45,6 +45,9 @@ function renderCart(cartList) {
             <div class="cart-card">
                 <p>${cart.name}</p>
                 <p>${cart.price}円</p>
+                <p>${cart.quantity}個</p>
+                <input class="product-quantity-input" type="number" data-id="${cart.id}">
+                <button class="product-quantity-button" data-id="${cart.id}">更新</button>
             </div>
         `);
     });
@@ -55,7 +58,23 @@ function addCart(productId) {
         return product.id === productId;
     });
 
+    product.quantity = 1;
     cartItems.push(product);
+
+    renderCart(cartItems);
+}
+
+$(document).on("click", ".product-quantity-button", function () {
+    const cartId = Number($(this).data("id"));
+    updateQuantity(cartId);
+});
+
+function updateQuantity(cartId) {
+    const cartItem = cartItems.find(function (item) {
+        return item.id === cartId;
+    })
+
+    cartItem.quantity = $(".product-quantity-input").val();
 
     renderCart(cartItems);
 }
